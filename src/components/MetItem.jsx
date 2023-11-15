1e4cbc3776a6828c2d02087602f858f1a1820411
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-11-15 16:10:39 +0100

    MetItem start styling Card

--- src/components/MetItem.jsx
+++ src/components/MetItem.jsx
@@ -3,11 +3,16 @@ import './MetItem.css'
 const MetItem = ({ result }) => {
 
     return (
-        <div>
-            <h4>{result.artistDisplayName}</h4>
-            <p>{result.title}</p>
-            <img src={result.primaryImageSmall} alt="" style={{ width: "500px" }} />
-        </div>
+        <article className='listItem'>
+            <div className='imageWrapper' style={{ backgroundImage: `url(${result.primaryImageSmall})` }} >
+                {/* <img src={result.primaryImageSmall} alt="" /> */}
+            </div>
+            <div className='infoWrapper'>
+                <h4 className='cardArtist'>{result.artistDisplayName}</h4>
+                <p className='cardTitle'>{result.title}</p>
+            </div>
+
+        </article>
     );
 }
 
