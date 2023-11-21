1d336265166d2713003c6b4cff5c9cd152e3594c
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-11-21 20:35:59 +0100

    changed HeaderImg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import './Header.css'
+import logo from '../assets/img/MetLogo.png'
 
 const Header = () => {
     return (
         <header className='HeaderContainer'>
             <div className='metLogoContainer'>
-                <img src="/MetLogo.png" alt="Logo" />
+                <img src={logo} alt="Logo" />
             </div>
             <h1>The Metropolitan Museum of Modern Art</h1>
-            <div className="HeaderImgContainer" style={{ backgroundImage: `url(/met-dark.jpeg)` }}>
+            <div className="HeaderImgContainer" style={{ backgroundImage: `url(https://images.metmuseum.org/CRDImages/as/original/DP-15583-014.jpg)` }}>
                 {/* <img src="https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg" alt="Wheat Field with Cypresses" /> */}
                 <p>Explore <br /> the Collection <br /> virtually</p>
             </div>
@@ -18,3 +19,4 @@ const Header = () => {
 export default Header;
 
 /*style={{ backgroundImage: 'url(https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg)' }}*/
+// style={{ backgroundImage: `url(https://images.metmuseum.org/CRDImages/ad/original/ap66.142.jpg)` }}
