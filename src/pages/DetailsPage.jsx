f4248428fa0ccfa3495c4846b01e8decffb1d2c4
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-11-28 19:41:42 +0100

    start adding details to DetailsPage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,16 +1,25 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
-import './DetailsPage.css'
+import './DetailsPage.css';
+
+
 
 const DetailsPage = () => {
 
     const location = useLocation()
     console.log(location);
 
+    const artwork = location.state;
+
     return (
         <div>
-            <h1>hier ist Details zu {location.state.title}</h1>
-            <img src={location.state.primaryImage} alt="" />
+            <h1>Title: {artwork.title}</h1>
+            <h2>Artist: {artwork.artistDisplayName}</h2>
+            <p>Medium: {artwork.medium}</p>
+            <p>Measurements (h/w): {artwork.measurements[0].elementMeasurements.Height} cm/ {artwork.measurements[0].elementMeasurements.Width} cm</p>
+            <p>Date: {artwork.objectDate}</p>
+            <Link to={artwork.primaryImage}><img src={artwork.primaryImageSmall} alt="" /></Link>
+
         </div>
     );
 }
